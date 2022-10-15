cfb7de6de97beaeed67909a33d425b4d5543f95c
qlitre <kuri0403@gmail.com>
2022-10-15 14:22:28 +0900

    create array utils

---
+++ qlitreutils/array/utils.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple
+
+
+def get_connected_value_list(a_list: List[Tuple[int, ...]], start_value: int) -> List[int]:
+    """
+    タプルのリストを受け取り、繋がっている要素をリストにして返す
+    例えばある地点からスタートして、どこまでたどり着けるか調べる
+    :param a_list ex.[(1, 2), (1, 3), (3, 6), (4, 5)]
+    :param start_value 最初に調べる値、例えばスタート地点
+    :return [1,2,3,6]
+    """
+    check_remains = [start_value]
+    return_values = []
+    while True:
+        check_value = check_remains.pop(0)
+        for item in a_list:
+            if check_value not in item:
+                continue
+            for elm in item:
+                if elm not in return_values:
+                    return_values.append(elm)
+                    check_remains.append(elm)
+        if not check_remains:
+            break
+    return return_values
---
+++ tests/test_array/test_utils.py
@@ -0,0 +1,14 @@
+from qlitreutils.array import utils
+
+
+def test_get_connected_value_list():
+    a_list = [(1, 2), (1, 3), (3, 6), (4, 5)]
+    return_list = utils.get_connected_value_list(a_list, 1)
+    assert return_list == [1, 2, 3, 6]
+    return_list = utils.get_connected_value_list(a_list, 3)
+    return_list.sort()
+    assert return_list == [1, 2, 3, 6]
+    a_list = [(1, 2), (1, 3), (3, 6), (4, 5), (6, 7, 8), (9,)]
+    return_list = utils.get_connected_value_list(a_list, 3)
+    return_list.sort()
+    assert return_list == [1, 2, 3, 6, 7, 8]
