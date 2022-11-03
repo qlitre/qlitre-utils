e252756f25719ca3ddfd57b9d381574bcf1a8a0e
qlitre <kuri0403@gmail.com>
2022-11-04 00:46:32 +0900

    add binary search

--- src/qlitreutils/search/utils.py
+++ src/qlitreutils/search/utils.py
@@ -15,3 +15,27 @@ def get_nearest_value_in_list(an_iterable: list, target: int) -> int:
         return an_iterable[index - 1]
     else:
         return an_iterable[index]
+
+
+def binary_search(a_list: list, target: int) -> bool:
+    first = 0
+    last = len(a_list) - 1
+    while last >= first:
+        mid = (first + last) // 2
+        if a_list[mid] == target:
+            return True
+        else:
+            if target < a_list[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return False
+
+
+def binary_search_w_bisect(an_iterable: list, target: int) -> bool:
+    index = bisect_left(an_iterable, target)
+    if index >= len(an_iterable):
+        return False
+    if an_iterable[index] == target:
+        return True
+    return False
