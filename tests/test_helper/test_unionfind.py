cada1a4c9c127625010e66da59d8e461f73ac3b2
qlitre <kuri0403@gmail.com>
2022-12-04 13:00:43 +0900

    add unionfind multiple

--- tests/test_helper/test_unionfind.py
+++ tests/test_helper/test_unionfind.py
@@ -1,29 +1,42 @@
 from src.qlitreutils.helper import unionfind
 
 
-def test_unionfind():
+def test_unionfind_simple():
     n = 6
-    uf = unionfind.UnionFind(n)
-    assert uf.parents == [-1] * n
-    assert uf.group_count() == 6
-    uf.union(1, 2)
-    uf.union(3, 4)
-    assert uf.members(1) == [1, 2]
-    assert uf.members(2) == [1, 2]
-    assert uf.members(3) == [3, 4]
-    assert uf.members(4) == [3, 4]
-    assert uf.group_count() == 4
-    uf.union(5, 1)
-    assert uf.members(1) == [1, 2, 5]
-    assert uf.members(2) == [1, 2, 5]
-    assert uf.members(5) == [1, 2, 5]
-    assert uf.members(3) == [3, 4]
-    assert uf.members(4) == [3, 4]
-    assert uf.roots() == [0, 1, 3]
-    assert uf.size(3) == 2
-    assert uf.size(1) == 3
-    assert uf.same(3, 4)
-    assert uf.same(1, 5)
-    assert uf.find(4) == 3
-    assert uf.find(5) == 1
-    assert uf.all_group_members() == {0: [0], 1: [1, 2, 5], 3: [3, 4]}
+    uf_simple = unionfind.UnionFindSimple(n)
+    assert uf_simple.parents == [-1] * n
+    assert uf_simple.group_count() == 6
+    uf_simple.union(1, 2)
+    uf_simple.union(3, 4)
+    assert uf_simple.members(1) == [1, 2]
+    assert uf_simple.members(2) == [1, 2]
+    assert uf_simple.members(3) == [3, 4]
+    assert uf_simple.members(4) == [3, 4]
+    assert uf_simple.group_count() == 4
+    uf_simple.union(5, 1)
+    assert uf_simple.members(1) == [1, 2, 5]
+    assert uf_simple.members(2) == [1, 2, 5]
+    assert uf_simple.members(5) == [1, 2, 5]
+    assert uf_simple.members(3) == [3, 4]
+    assert uf_simple.members(4) == [3, 4]
+    assert uf_simple.roots() == [0, 1, 3]
+    assert uf_simple.size(3) == 2
+    assert uf_simple.size(1) == 3
+    assert uf_simple.same(3, 4)
+    assert uf_simple.same(1, 5)
+    assert uf_simple.find(4) == 3
+    assert uf_simple.find(5) == 1
+    assert uf_simple.all_group_members() == {0: [0], 1: [1, 2, 5], 3: [3, 4]}
+
+
+def test_unionfind_multiple():
+    uf_multi = unionfind.UnionFindMultiple()
+    item1 = (1, 1)
+    item2 = (1, 2)
+    uf_multi.union(item1, item2)
+    members = uf_multi.all_group_members()
+    assert members == {(1, 1): {(1, 1), (1, 2)}}
+    item1 = (1, 1)
+    item2 = (3, 3)
+    uf_multi.union(item1, item2)
+    assert members == {(1, 1): {(1, 1), (1, 2), (3, 3)}}
