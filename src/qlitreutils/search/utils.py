5cf50cc183307186a841f28d65a8ae1cb3d6dfab
qlitre <kuri0403@gmail.com>
2022-12-30 15:56:57 +0900

    add meguru bisect

--- src/qlitreutils/search/utils.py
+++ src/qlitreutils/search/utils.py
@@ -39,3 +39,23 @@ def binary_search_w_bisect(an_iterable: list, target: int) -> bool:
     if an_iterable[index] == target:
         return True
     return False
+
+
+def meguru_bisect(ng, ok):
+    """
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    """
+
+    def is_ok(arg):
+        pass
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
